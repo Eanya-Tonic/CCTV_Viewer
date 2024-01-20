6ec64f55268962002a26f61ea270e338249f2fd1
3anya-T0n1c <khufujason@outlook.com>
2024-01-20 14:59:47 +0800

    修复清晰度选择导致的无法全屏和播放的问题

--- app/src/main/java/com/eanyatonic/cctvViewer/MainActivity.java
+++ app/src/main/java/com/eanyatonic/cctvViewer/MainActivity.java
@@ -184,46 +184,51 @@ public class MainActivity extends AppCompatActivity {
 
                     String script =
                             """
-                            // 定义休眠函数
-                            function sleep(ms) {
-                                return new Promise(resolve => setTimeout(resolve, ms));
-                            }
-                
-                            // 页面加载完成后执行 JavaScript 脚本
-                            let interval=setInterval(async function executeScript() {
-                                console.log('页面加载完成！');
-                
-                                // 休眠 1000 毫秒（1秒）
-                                await sleep(1000);
-                
-                                // 休眠 50 毫秒
-                                await sleep(50);
-                
-                                console.log('点击分辨率按钮');
-                                var elem = document.querySelector('#resolution_item_720_player');
-                                elem.click();
-                
-                                // 休眠 50 毫秒
-                                await sleep(50);
-                
-                                console.log('设置音量并点击音量按钮');
-                                var btn = document.querySelector('#player_sound_btn_player');
-                                btn.setAttribute('volume', 100);
-                                // btn.click();
-                                // btn.click();
-                                // btn.click();
-                
-                                // 休眠 50 毫秒
-                                await sleep(50);
-                
-                                console.log('点击全屏按钮');
-                                var fullscreenBtn = document.querySelector('#player_pagefullscreen_yes_player');
-                                fullscreenBtn.click();
-                                clearInterval(interval);
-                            }, 3000);
-                
-                            executeScript();
-                            """;
+                                    // 定义休眠函数
+                                    function sleep(ms) {
+                                        return new Promise(resolve => setTimeout(resolve, ms));
+                                    }
+                                                    
+                                    // 页面加载完成后执行 JavaScript 脚本
+                                    let interval=setInterval(async function executeScript() {
+                                        console.log('页面加载完成！');
+                                                    
+                                        // 休眠 1000 毫秒（1秒）
+                                        await sleep(1000);
+                                                    
+                                        // 休眠 50 毫秒
+                                        await sleep(50);
+                                                    
+                                        console.log('设置音量并点击音量按钮');
+                                        var btn = document.querySelector('#player_sound_btn_player');
+                                        btn.setAttribute('volume', 100);
+                                        // btn.click();
+                                        // btn.click();
+                                        // btn.click();
+                                                    
+                                        // 休眠 50 毫秒
+                                        await sleep(50);
+                                                    
+                                        console.log('点击全屏按钮');
+                                        var fullscreenBtn = document.querySelector('#player_pagefullscreen_yes_player');
+                                        fullscreenBtn.click();
+                                        
+                                        // 休眠 50 毫秒
+                                        await sleep(50);
+                                                    
+                                        console.log('点击分辨率按钮');
+                                        var elem = document.querySelector('#resolution_item_720_player');
+                                        try {
+                                            elem.click();
+                                            }
+                                        catch (error) {
+                                            clearInterval(interval);
+                                            }
+                                        clearInterval(interval);
+                                    }, 3000);
+                                                    
+                                    executeScript();
+                                    """;
                     view.evaluateJavascript(script, null);
 
                     new Handler().postDelayed(() -> {
