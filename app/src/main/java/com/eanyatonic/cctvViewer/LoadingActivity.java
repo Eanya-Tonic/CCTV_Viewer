b029c5db7d58d7205f4b90534e0b53c3a8dc3606
3anya-T0n1c <khufujason@outlook.com>
2024-10-06 13:42:32 +0800

    增加安装界面延时

--- app/src/main/java/com/eanyatonic/cctvViewer/LoadingActivity.java
+++ app/src/main/java/com/eanyatonic/cctvViewer/LoadingActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.content.Intent;
+import android.os.Handler;
 
 import com.tencent.smtt.sdk.QbSdk;
 import com.tencent.smtt.sdk.TbsListener;
@@ -20,32 +21,34 @@ public class LoadingActivity extends AppCompatActivity {
         FileUtils.copyAssets(getApplicationContext(), "045738_x5.tbs.apk",
                 FileUtils.getTBSFileDir(getApplicationContext()).getPath() + "/045738_x5.tbs.apk");
 
-        // 安装TBS内核
-        QbSdk.reset(getApplicationContext());
-        QbSdk.installLocalTbsCore(getApplicationContext(), 45738,
-                FileUtils.getTBSFileDir(getApplicationContext()).getPath() + "/045738_x5.tbs.apk");
-        QbSdk.setTbsListener(new TbsListener() {
-            @Override
-            public void onDownloadFinish(int i) {
-                Log.e("TAG", "进行了tbs:onDownloadFinish " + i);
-            }
+        new Handler().postDelayed(() -> {
+            // 安装TBS内核
+            QbSdk.reset(getApplicationContext());
+            QbSdk.installLocalTbsCore(getApplicationContext(), 45738,
+                    FileUtils.getTBSFileDir(getApplicationContext()).getPath() + "/045738_x5.tbs.apk");
+            QbSdk.setTbsListener(new TbsListener() {
+                @Override
+                public void onDownloadFinish(int i) {
+                    Log.e("TAG", "进行了tbs:onDownloadFinish " + i);
+                }
 
-            @Override
-            public void onDownloadProgress(int i) {
-                Log.e("TAG", "进行了tbs:onDownloadProgress " + i);
-            }
+                @Override
+                public void onDownloadProgress(int i) {
+                    Log.e("TAG", "进行了tbs:onDownloadProgress " + i);
+                }
 
-            @Override
-            public void onInstallFinish(int i) {
-                Log.e("TAG", "进行了tbs:onInstallFinish " + i);
-                boolean canLoadX5 = QbSdk.canLoadX5(getApplicationContext());
-                Log.d("canLoadX5", String.valueOf(canLoadX5));
-                Log.d("versionX5",String.valueOf(QbSdk.getTbsVersion(getApplicationContext())));
-                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                finish(); // 销毁 LoadingActivity
-            }
-        });
+                @Override
+                public void onInstallFinish(int i) {
+                    Log.e("TAG", "进行了tbs:onInstallFinish " + i);
+                    boolean canLoadX5 = QbSdk.canLoadX5(getApplicationContext());
+                    Log.d("canLoadX5", String.valueOf(canLoadX5));
+                    Log.d("versionX5", String.valueOf(QbSdk.getTbsVersion(getApplicationContext())));
+                    Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish(); // 销毁 LoadingActivity
+                }
+            });
+        }, 6000);
     }
 }
